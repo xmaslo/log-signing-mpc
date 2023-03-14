dcc148bc1f47166102c8e386f06dadf05039d538
David Rajnoha <drajnoha@redhat.com>
2023-03-14 15:18:41 +0100

    Add support for resending messages and a new endpoint for initiating the broadcast loop
    The commit adds a new endpoint init_room to the Rocket server, which starts the broadcast loop in the Room state managed by the server. The broadcast loop sends all the messages received on the outgoing_stream to all the servers by resending the message with Resended appended to it. The commit also adds a new OutgoingSink struct that abstracts a sink that can send messages to the outgoing_stream. The OutgoingSink struct is used to manage the outgoing message sink and is included in the Rocket state as a managed object.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,40 @@
-use futures::{Sink, SinkExt, Stream, StreamExt, TryStreamExt};
-use futures::channel::mpsc::{unbounded, UnboundedReceiver, UnboundedSender};
+use futures::{Sink, SinkExt, Stream, StreamExt};
 
-use std::sync::{
-    atomic::{AtomicU16},
-    Arc, Mutex,
-};
-use rocket::data::ToByteUnit;
+use std::sync::{Arc};
 use rocket::http::Status;
 use rocket::State;
 use tokio::sync::{RwLock};
+
+#[rocket::post("/send_broadcast", data = "<message>")]
+async fn send_broadcast(outgoing_sink: &State<OutgoingSink>, message: String) -> Status {
+    outgoing_sink.receive(message).await;
+    Status::Ok
+}
+
+struct OutgoingSink {
+    outgoing_sink: Arc<RwLock<Box<dyn Sink<String, Error=futures::channel::mpsc::SendError> + Send + Sync + Unpin>>>,
+}
+
+impl OutgoingSink {
+    pub fn new(sink: Box<dyn Sink<String, Error=futures::channel::mpsc::SendError> + Send + Sync + Unpin>) -> Self {
+        Self {
+            outgoing_sink: Arc::new(RwLock::new(sink))
+        }
+    }
+
+    pub async fn receive(&self, message: String) {
+        let mut guard = self.outgoing_sink.write().await;
+        let mut sink = guard.as_mut();
+        sink.send(message).await.unwrap();
+    }
+}
+
+// Handling the sending messages from this server
+#[rocket::post("/init_room")]
+async fn init_room(room: &State<Room>) -> Status {
+    room.init_room().await;
+    Status::Ok
+}
 
 
 
@@ -21,13 +47,32 @@ async fn receive_broadcast(room: &State<Room>, message: String) -> Status {
 
 struct Room {
     receiving_sink: Arc<RwLock<Box<dyn Sink<String, Error = futures::channel::mpsc::SendError> + Send + Sync + Unpin>>>,
+    outgoing_stream: Arc<RwLock<Box<dyn Stream<Item = String> + Send + Sync + Unpin>>>,
+    client: reqwest::Client,
 }
 
 // TODO: Add the logic of multiple groups
 impl Room {
-    pub fn new(sink: Box<dyn Sink<String, Error = futures::channel::mpsc::SendError> + Send + Sync + Unpin>) -> Self {
+    pub fn new(sink: Box<dyn Sink<String, Error = futures::channel::mpsc::SendError> + Send + Sync + Unpin>,
+               stream: Box<dyn Stream<Item = String> + Send + Sync + Unpin>,) -> Self {
+
         Self {
             receiving_sink: Arc::new(RwLock::new(sink)),
+            outgoing_stream: Arc::new(RwLock::new(stream)),
+            client: reqwest::Client::new(),
+        }
+    }
+
+    pub async fn init_room(&self) {
+        loop {
+            match self.outgoing_stream.write().await.next().await {
+                Some(message) => {
+                    let message = format!("{} Resended", message);
+                    println!("Sending: {}", message);
+                    self.client.post("http://localhost:8000/receive_broadcast").body(message).send().await.unwrap();
+                }
+                None => break,
+            }
         }
     }
 
@@ -36,22 +81,27 @@ impl Room {
         let mut sink = guard.as_mut();
         sink.send(message).await.unwrap();
     }
+
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let (sink, mut stream) = futures::channel::mpsc::unbounded();
+    let (receiving_sink, mut receiving_stream) = futures::channel::mpsc::unbounded();
+    let (outgoing_sink, mut outgoing_stream) = futures::channel::mpsc::unbounded();
 
-    // The sink will be passed to the multisig library to work with it
+
+    // The receiving_stream will be passed to the multisig library to work with it
     tokio::spawn(async move {
-        while let Some(message) = stream.next().await {
+        while let Some(message) = receiving_stream.next().await {
             println!("Received: {}", message);
         }
     });
 
     rocket::build()
-        .mount("/", rocket::routes![receive_broadcast])
-        .manage(Room::new(Box::new(sink)))
+        .mount("/", rocket::routes![receive_broadcast, send_broadcast, init_room])
+        .manage(Room::new(Box::new(receiving_sink), Box::new(outgoing_stream)))
+        // The outgoing_sink will be passed to the multisig library to work with it
+        .manage(OutgoingSink::new(Box::new(outgoing_sink)))
         .launch()
         .await?;
 
