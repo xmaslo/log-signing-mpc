4bb2f67e30bd25de2aef3a88488c61146456b40d
Dávid Maslo <73maslo.david73@gmail.com>
2023-03-19 18:43:12 +0100

    alternative way

--- src/key_generator.rs
+++ src/key_generator.rs
@@ -1,9 +1,13 @@
 use std::fs::File;
 use std::io::{Write};
 use std::path::Path;
+use std::pin::Pin;
+use std::rc::Rc;
+use std::sync::Arc;
 use anyhow::{anyhow, Error};
 use curv::elliptic::curves::Secp256k1;
 use futures::{Sink, Stream, StreamExt};
+use futures::stream::Fuse;
 use multi_party_ecdsa::protocols::multi_party_ecdsa::gg_2020::state_machine::keygen::{Keygen, ProtocolMessage, LocalKey};
 use round_based::{AsyncProtocol, Msg};
 use tokio::sync::mpsc::{UnboundedReceiver, UnboundedSender};
@@ -12,32 +16,16 @@ use super::create_communication_channel::{SerializableMessage};
 const THRESHOLD: u16 = 2;
 const NUMBER_OF_PARTIES: u16 = 3;
 
-pub struct KeyGenerator {
+/*pub struct KeyGenerator {
     index: u16,
-    /*receiving_stream: Pin<&mut Fuse<dyn Stream<Item = Result<Msg<ProtocolMessage>, Error>>>>,
-    outgoing_sink: Box<dyn Sink<Msg<ProtocolMessage>, Error=anyhow::Error> + Send + Sync>*/
-}
+    receiving_stream: Fuse<Arc<dyn Stream<Item=Result<Msg<ProtocolMessage>, Error>> + Send + Sync>>,
+    outgoing_sink: dyn Sink<Msg<ProtocolMessage>, Error=Error> + Send + Sync
+}*/
 
 // let mut incoming: Pin<&mut Fuse<impl Stream<Item = Result<Msg<ProtocolMessage>, Error>>>>
 // let mut outgoing: Pin<&mut (impl Sink<Msg<ProtocolMessage>, Error = Error>)>
 
-impl KeyGenerator {
-    pub fn new(i: u16) -> KeyGenerator {
-        KeyGenerator{
-            index: i
-        }
-    }
-    /*pub fn new(i: u16,
-               rs: Box<dyn Stream<Item = Msg<ProtocolMessage>> + Send + Sync>,
-               os: Box<dyn Sink<Msg<ProtocolMessage>, Error=anyhow::Error> + Send + Sync>) -> KeyGenerator {
-        KeyGenerator {
-            index: i,
-            receiving_stream: rs,
-            outgoing_sink: os
-        }
-    }*/
-
-    /*pub fn new(i: u16,
+/*pub fn new(i: u16,
                rs: Pin<&mut Fuse<impl Stream<Item = Result<Msg<ProtocolMessage>, Error>>>>,
                os: Pin<&mut (impl Sink<Msg<ProtocolMessage>, Error = Error>)>) -> KeyGenerator {
         KeyGenerator {
@@ -47,11 +35,36 @@ impl KeyGenerator {
         }
     }*/
 
-    pub async fn run(self, file_name: &Path,
-                     rs: impl Stream<Item=Result<Msg<ProtocolMessage>, Error>>+Sized,
-                     os: impl Sink<Msg<ProtocolMessage>, Error=Error>+Sized) {
-        let incoming = rs.fuse();
-        let outgoing = os;
+pub struct KeyGenerator<S, T>
+    where
+        S: Stream<Item=Result<Msg<ProtocolMessage>, Error>> + Send + Sync + 'static,
+        T: Sink<Msg<ProtocolMessage>, Error=Error> + Send + Sync + 'static,
+{
+    index: u16,
+    receiving_stream: Fuse<S>,
+    outgoing_sink: T,
+}
+
+impl<S, T> KeyGenerator<S, T>
+    where
+        S: Stream<Item=Result<Msg<ProtocolMessage>, Error>> + Send + Sync + 'static,
+        T: Sink<Msg<ProtocolMessage>, Error=Error> + Send + Sync + 'static,
+{
+    pub fn new(
+        i: u16,
+        rs: S,
+        os: T,
+    ) -> KeyGenerator<S, T> {
+        KeyGenerator {
+            index: i,
+            receiving_stream: rs.fuse(),
+            outgoing_sink: os,
+        }
+    }
+
+    pub async fn run(self, file_name: &Path) -> usize {
+        let incoming = self.receiving_stream;
+        let outgoing = self.outgoing_sink;
         tokio::pin!(incoming);
         tokio::pin!(outgoing);
 
@@ -63,10 +76,25 @@ impl KeyGenerator {
 
         let local_key: LocalKey<Secp256k1> = results.unwrap();
 
-        self.generate_file(file_name, &local_key);
+        println!("Writing generated key into {:?}", file_name);
+        if file_name.exists() {
+            println!("{:?} already exists. Removing it...", file_name);
+        }
+        let open_result = File::create(file_name);
+        let mut file = match open_result {
+            Ok(f) => f,
+            Err(error) => panic!("Problem with opening file {:?}", error)
+        };
+
+        let output = serde_json::to_vec_pretty(&local_key);
+
+        let write_result = file.write(output.unwrap().as_ref()).unwrap();
+
+        write_result
+        // self.generate_file(file_name, &local_key);
     }
 
-    fn generate_file(&self, file_name: &Path, result: &LocalKey<Secp256k1>) -> usize {
+    /*fn generate_file(&self, file_name: &Path, result: &LocalKey<Secp256k1>) -> usize {
         println!("Writing generated key into {:?}", file_name);
         if file_name.exists() {
             println!("{:?} already exists. Removing it...", file_name);
@@ -82,5 +110,5 @@ impl KeyGenerator {
         let write_result = file.write(output.unwrap().as_ref()).unwrap();
 
         write_result
-    }
+    }*/
 }
