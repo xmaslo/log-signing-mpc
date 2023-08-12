e83d10b41d12a989d2f73556661e7bd92bb89c52
Dávid Maslo <73maslo.david73@gmail.com>
2023-08-12 18:36:35 +0200

    added 10s timeouts on post requests

--- integration-tests/common.py
+++ integration-tests/common.py
@@ -1,4 +1,8 @@
+import aiohttp
+
+
 async def send_post_request(session, url, payload):
-    async with session.post(url=url, data=payload) as response:
+    timeout = aiohttp.ClientTimeout(total=10)
+    async with session.post(url=url, data=payload, timeout=timeout) as response:
         return response.status
 
