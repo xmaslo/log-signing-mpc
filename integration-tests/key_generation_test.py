b2047e772a64af97dc95ee490d4c603831db9881
Dávid Maslo <73maslo.david73@gmail.com>
2023-08-12 19:33:44 +0200

    moved asserts outside trigger_keygen_endpoint()

--- integration-tests/key_generation_test.py
+++ integration-tests/key_generation_test.py
@@ -4,7 +4,7 @@ from setup_for_tests import *
 from common import send_post_request
 
 
-async def trigger_keygen_endpoint(keys_already_generated):
+async def trigger_keygen_endpoint():
     payload1 = URL1 + "," + URL2
     payload2 = URL2 + "," + URL0
     payload3 = URL1 + "," + URL0
@@ -16,23 +16,18 @@ async def trigger_keygen_endpoint(keys_already_generated):
             send_post_request(session, f"{BASE_URL}:{SERVER_PORT2}/key_gen/1", payload3),
         ]
 
-        server1_res, server2_res, server3_res = await asyncio.gather(*tasks)
-
-    if not keys_already_generated:
-        assert server1_res == 200
-        assert server2_res == 200
-        assert server3_res == 200
-    else:
-        assert server1_res == 403
-        assert server2_res == 403
-        assert server3_res == 403
+        return await asyncio.gather(*tasks)
 
 
 def test_keygen_no_keys():
     """
     Tests that keys are correctly generated on a newly set up machines.
     """
-    asyncio.run(trigger_keygen_endpoint(False))
+    server1_res, server2_res, server3_res = asyncio.run(trigger_keygen_endpoint())
+
+    assert server1_res == 200
+    assert server2_res == 200
+    assert server3_res == 200
 
 
 def test_keygen_keys_already_present():
@@ -41,4 +36,8 @@ def test_keygen_keys_already_present():
     possible to regenerate new (and overwrite old ones) using the key
     generation endpoint.
     """
-    asyncio.run(trigger_keygen_endpoint(True))
+    server1_res, server2_res, server3_res = asyncio.run(trigger_keygen_endpoint())
+
+    assert server1_res == 403
+    assert server2_res == 403
+    assert server3_res == 403
