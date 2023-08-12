3bed7706db9985b0e8df60c75fb9c26a0659f9bc
Dávid Maslo <73maslo.david73@gmail.com>
2023-08-12 10:37:40 +0200

    add some spaces for god sake

--- integration-tests/setup_for_tests.py
+++ integration-tests/setup_for_tests.py
@@ -1,8 +1,11 @@
-IS_DOCKER = True
+IS_DOCKER = False
+
 BASE_URL = "http://localhost"
+
 SERVER_PORT1 = "8000"
 SERVER_PORT2 = "8001"
 SERVER_PORT3 = "8002"
+
 if IS_DOCKER:
     URL0 = "la1:3000"
     URL1 = "la2:3001"
