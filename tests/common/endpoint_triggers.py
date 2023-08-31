4d107972610816dd7e8be3a466ac93e323247c5d
Dávid Maslo <73maslo.david73@gmail.com>
2023-08-31 20:07:44 +0200

    test for parallel signatures added

--- tests/common/endpoint_triggers.py
+++ tests/common/endpoint_triggers.py
@@ -32,6 +32,28 @@ async def trigger_sign_endpoint(session, participating_parties, urls, ports, tim
     return await asyncio.gather(*tasks)
 
 
+async def trigger_sign_endpoint_in_multiple_rooms(session,
+                                                  participating_parties,
+                                                  urls,
+                                                  ports,
+                                                  timestamp,
+                                                  data_list,
+                                                  rooms):
+    assert len(data_list) == len(rooms)
+
+    tasks = []
+
+    for count, room in enumerate(rooms):
+        data = data_list[count].encode().hex()
+        payload1 = f"{str(participating_parties[1])}," + f"{urls[1]}," + data + "," + timestamp
+        payload2 = f"{str(participating_parties[0])}," + f"{urls[0]}," + data + "," + timestamp
+
+        tasks.append(send_post_request(session, f"{BASE_URL}:{ports[0]}/sign/{room}", payload1))
+        tasks.append(send_post_request(session, f"{BASE_URL}:{ports[1]}/sign/{room}", payload2))
+
+    return await asyncio.gather(*tasks)
+
+
 async def trigger_verify_endpoint(url, data_to_sign, signature, timestamp):
     data_to_sign = data_to_sign.encode().hex()
     payload = f"{signature.hex()},{data_to_sign},{timestamp}"
--- tests/integration_tests/signing_test.py
+++ tests/integration_tests/signing_test.py
@@ -2,7 +2,7 @@ import asyncio
 import aiohttp
 from common.setup_for_tests import *
 from common.common import get_current_timestamp
-from common.endpoint_triggers import trigger_sign_endpoint
+from common.endpoint_triggers import trigger_sign_endpoint, trigger_sign_endpoint_in_multiple_rooms
 
 
 DATA_TO_SIGN = "{some,arbitrary,data,to,sign}"
@@ -20,6 +20,19 @@ async def sign_data(participating_parties, urls, ports, timestamp, data, room):
                                                                )
 
         return server1_res, server2_res
+
+
+async def sign_data_in_parallel(participating_parties, urls, ports, timestamp, data_list, rooms):
+    async with aiohttp.ClientSession() as session:
+        responses = await trigger_sign_endpoint_in_multiple_rooms(session,
+                                                                  participating_parties,
+                                                                  urls,
+                                                                  ports,
+                                                                  timestamp,
+                                                                  data_list,
+                                                                  rooms
+                                                                  )
+        return responses
 
 
 def test_signing_on_all_party_combinations():
@@ -67,3 +80,34 @@ def test_signing_on_all_party_combinations():
     )
     assert server1_res[0] == 200
     assert server2_res[0] == 200
+
+
+def run_parallel_signatures(number_of_parallel_signatures):
+    timestamp = get_current_timestamp()
+
+    responses = asyncio.run(
+        sign_data_in_parallel(
+            [2, 3],
+            [URL1, URL2],
+            [SERVER_PORT1, SERVER_PORT2],
+            timestamp,
+            [DATA_TO_SIGN for _ in range(number_of_parallel_signatures)],
+            [i for i in range(1, number_of_parallel_signatures + 1)]
+        )
+    )
+
+    grouped_responses = []
+    for i in range(0, len(responses), 2):
+        grouped_responses.append((responses[i], responses[i + 1]))
+
+    return grouped_responses
+
+
+def test_parallel_signatures():
+    number_of_parallel_signatures = 2
+
+    responses = run_parallel_signatures(number_of_parallel_signatures)
+
+    for i in range(0, number_of_parallel_signatures, 2):
+        assert responses[i][0][0] == 200
+        assert responses[i][1][0] == 200
