3e85f3668dcfc4bbd9561f9f6968a2fe7f4ee955
Dávid Maslo <485351@mail.muni.cz>
2023-10-07 12:13:53 +0200

    fixed unused number of servers

--- tests/integration_tests/key_generation_test.py
+++ tests/integration_tests/key_generation_test.py
@@ -4,7 +4,7 @@ from common.endpoint_triggers import trigger_keygen_endpoint
 
 
 def generate_keys(n, expected_err_code):
-    results = asyncio.run(trigger_keygen_endpoint(3))
+    results = asyncio.run(trigger_keygen_endpoint(n))
 
     for result in results:
         assert result[0] == expected_err_code
