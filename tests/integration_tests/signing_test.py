ea00ba40283e5d7adca000cf8b41c9234c9ab788
Dávid Maslo <485351@mail.muni.cz>
2023-10-20 17:43:16 +0200

    splitting signing tests into classes

--- tests/integration_tests/signing_test.py
+++ tests/integration_tests/signing_test.py
@@ -7,63 +7,64 @@ from common.signatures import sign_data, run_parallel_signatures
 DATA_TO_SIGN = "{some,arbitrary,data,to,sign}"
 
 
-def test_signing_14():
-    timestamp = get_current_timestamp()
+class TestSigning13:
+    def test_signing_on_all_party_combinations(self):
+        """
+        Verifies that all signing combinations, namely
+        [1,2], [1,3], and [2,3] work.
+        """
+        timestamp = get_current_timestamp()
 
-
-def test_signing_on_all_party_combinations_13():
-    """
-    Verifies that all signing combinations, namely
-    [1,2], [1,3], and [2,3] work.
-    """
-    timestamp = get_current_timestamp()
-
-    server1_res, server2_res = asyncio.run(
-        sign_data(
-            [1, 2],
-            [URL1, URL2],
-            [SERVER_PORT1, SERVER_PORT2],
-            timestamp,
-            DATA_TO_SIGN,
-            1
+        server1_res, server2_res = asyncio.run(
+            sign_data(
+                [1, 2],
+                [URL1, URL2],
+                [SERVER_PORT1, SERVER_PORT2],
+                timestamp,
+                DATA_TO_SIGN,
+                1
+            )
         )
-    )
-    assert server1_res[0] == 200
-    assert server2_res[0] == 200
+        assert server1_res[0] == 200
+        assert server2_res[0] == 200
 
-    server1_res, server2_res = asyncio.run(
-        sign_data(
-            [1, 3],
-            [URL1, URL3],
-            [SERVER_PORT1, SERVER_PORT3],
-            timestamp,
-            DATA_TO_SIGN,
-            1
+        server1_res, server2_res = asyncio.run(
+            sign_data(
+                [1, 3],
+                [URL1, URL3],
+                [SERVER_PORT1, SERVER_PORT3],
+                timestamp,
+                DATA_TO_SIGN,
+                1
+            )
         )
-    )
-    assert server1_res[0] == 200
-    assert server2_res[0] == 200
+        assert server1_res[0] == 200
+        assert server2_res[0] == 200
 
-    server1_res, server2_res = asyncio.run(
-        sign_data(
-            [2, 3],
-            [URL2, URL3],
-            [SERVER_PORT2, SERVER_PORT3],
-            timestamp,
-            DATA_TO_SIGN,
-            1
+        server1_res, server2_res = asyncio.run(
+            sign_data(
+                [2, 3],
+                [URL2, URL3],
+                [SERVER_PORT2, SERVER_PORT3],
+                timestamp,
+                DATA_TO_SIGN,
+                1
+            )
         )
-    )
-    assert server1_res[0] == 200
-    assert server2_res[0] == 200
+        assert server1_res[0] == 200
+        assert server2_res[0] == 200
+
+    def test_parallel_signatures_13(self):
+        number_of_parallel_signatures = 2
+
+        responses = run_parallel_signatures(number_of_parallel_signatures,
+                                            [DATA_TO_SIGN for _ in range(number_of_parallel_signatures)])
+
+        for i in range(0, number_of_parallel_signatures, 2):
+            assert responses[i][0][0] == 200
+            assert responses[i][1][0] == 200
 
 
-def test_parallel_signatures_13():
-    number_of_parallel_signatures = 2
-
-    responses = run_parallel_signatures(number_of_parallel_signatures,
-                                        [DATA_TO_SIGN for _ in range(number_of_parallel_signatures)])
-
-    for i in range(0, number_of_parallel_signatures, 2):
-        assert responses[i][0][0] == 200
-        assert responses[i][1][0] == 200
+class TestSigning14:
+    def test_signing(self):
+        timestamp = get_current_timestamp()
