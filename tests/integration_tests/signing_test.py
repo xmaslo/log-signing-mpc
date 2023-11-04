daebd9775032cbb905eca885463ead130e3b3523
Dávid Maslo <485351@mail.muni.cz>
2023-11-04 15:52:54 +0100

    added one more test to signing so I have function to run a single signature

--- tests/integration_tests/signing_test.py
+++ tests/integration_tests/signing_test.py
@@ -9,6 +9,22 @@ DATA_TO_SIGN2 = "{another,arbitrary,data}"
 
 
 class TestSigning13:
+    def test_sign_data(self):
+        timestamp = get_current_timestamp()
+
+        responses = asyncio.run(
+            sign_data(
+                [1, 2],
+                [URL1, URL2],
+                [SERVER_PORT1, SERVER_PORT2],
+                timestamp,
+                DATA_TO_SIGN1,
+                1
+            )
+        )
+        assert responses[0][0] == 200
+        assert responses[1][0] == 200
+
     def test_signing_on_all_party_combinations(self):
         timestamp = get_current_timestamp()
 
@@ -66,6 +82,23 @@ class TestSigning13:
 
 
 class TestSigning24:
+    def test_sign_data(self):
+        timestamp = get_current_timestamp()
+
+        responses = asyncio.run(
+            sign_data(
+                [1, 2, 3],
+                [URL1, URL2, URL3],
+                [SERVER_PORT1, SERVER_PORT2, SERVER_PORT3],
+                timestamp,
+                DATA_TO_SIGN1,
+                1
+            )
+        )
+        assert responses[0][0] == 200
+        assert responses[1][0] == 200
+        assert responses[2][0] == 200
+
     def test_signing_on_all_party_combinations(self):
         timestamp = get_current_timestamp()
 
