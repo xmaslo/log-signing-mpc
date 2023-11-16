33d8587647f9c9b4bd16f9551ddd688e97f0c10b
Dávid Maslo <485351@mail.muni.cz>
2023-11-16 16:48:23 +0100

    found even better place for get_keygen_payloads()

--- tests/common/endpoint_triggers.py
+++ tests/common/endpoint_triggers.py
@@ -2,23 +2,7 @@ import asyncio
 import aiohttp
 from common.setup_for_tests import *
 from common.common import send_post_request
-from common.create_payload import create_sign_payload, get_payloads_layout
-
-
-def get_keygen_payloads(n, is_docker):
-    payloads = []
-    for i in range(n):
-        payload = ""
-        for j in range(n):
-            if i != j:
-                if is_docker:
-                    payload += f"la{j + 1}:300{j + 1},"
-                else:
-                    payload += f"127.0.0.1:300{j + 1},"
-
-        payloads.append(payload[:-1])  # remove trailing ','
-
-    return payloads
+from common.create_payload import get_keygen_payloads, create_sign_payload, get_payloads_layout
 
 
 async def trigger_keygen_endpoint(n):
