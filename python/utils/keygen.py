1e0e409a9c1add07f5bcfdd30adbccf72f6494d3
Dávid Maslo <485351@mail.muni.cz>
2023-11-17 12:20:44 +0100

    moved some functionality out of keygen tests

---
+++ python/utils/keygen.py
@@ -0,0 +1,9 @@
+import asyncio
+from python.utils.endpoint_triggers import trigger_keygen_endpoint
+
+
+def generate_keys(n, expected_err_code):
+    results = asyncio.run(trigger_keygen_endpoint(n))
+
+    for result in results:
+        assert result[0] == expected_err_code
