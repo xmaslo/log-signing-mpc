17555e8525ee6256e98bfa62e32ba159aa59fe8b
Dávid Maslo <485351@mail.muni.cz>
2023-12-09 16:03:11 +0100

    extracted common functionality outside of the verification tests

--- evaluation/tests/verification_test.py
+++ evaluation/tests/verification_test.py
@@ -4,21 +4,29 @@ from evaluation.utils.common import get_current_timestamp
 from evaluation.utils.endpoint_triggers import trigger_verify_endpoint
 from evaluation.utils.signatures import get_signature
 
+
 DATA_TO_SIGN = "{some,arbitrary,data,to,sign}"
+
+
+def compute_signature(n, data_to_sign):
+    timestamp = get_current_timestamp()
+    internal_urls = get_inter_comm_urls(n, IS_DOCKER)
+    outside_ports = get_ports(n, 8000)
+
+    signature = asyncio.run(
+        get_signature(timestamp,
+                      list(range(1, n+1)),
+                      internal_urls,
+                      outside_ports,
+                      data_to_sign))
+
+    return signature, timestamp
 
 
 class TestVerify13:
     def test_verify_signature_on_all_parties(self):
-        timestamp = get_current_timestamp()
-        internal_urls = get_inter_comm_urls(3, IS_DOCKER)
+        signature, timestamp = compute_signature(2, DATA_TO_SIGN)
         outside_ports = get_ports(3, 8000)
-
-        signature = asyncio.run(
-            get_signature(timestamp,
-                          [1, 2],
-                          [internal_urls[0], internal_urls[1]],
-                          [outside_ports[0], outside_ports[1]],
-                          DATA_TO_SIGN))
 
         response1 = asyncio.run(
             trigger_verify_endpoint(BASE_URL_HTTP + f":{outside_ports[0]}", DATA_TO_SIGN, signature, timestamp))
@@ -34,16 +42,8 @@ class TestVerify13:
 
 class TestVerify24:
     def test_verify_signature_on_all_parties(self):
-        timestamp = get_current_timestamp()
-        internal_urls = get_inter_comm_urls(4, IS_DOCKER)
+        signature, timestamp = compute_signature(3, DATA_TO_SIGN)
         outside_ports = get_ports(4, 8000)
-
-        signature = asyncio.run(
-            get_signature(timestamp,
-                          [1, 3, 4],
-                          [internal_urls[0], internal_urls[2], internal_urls[3]],
-                          [outside_ports[0], outside_ports[2], outside_ports[3]],
-                          DATA_TO_SIGN))
 
         response1 = asyncio.run(
             trigger_verify_endpoint(BASE_URL_HTTP + f":{outside_ports[0]}", DATA_TO_SIGN, signature, timestamp))
